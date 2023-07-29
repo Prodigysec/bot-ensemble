efc12a7b46cb76df1f0a257cb2f3b7712270f3b5
Ian Raburu <williamraburu@gmail.com>
2023-07-29 16:30:15 +0300

    Create BotArmy component and set useBotContext

---
+++ src/components/BotArmy.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BotProfile from './BotProfile';
+import { useBotContext } from './BotContext';
+
+function BotArmy() {
+    const { enlistedBots } = useBotContext();
+
+    return (
+        
+    );
+}
+
+export default BotArmy;
