4916e08cfe623d1325096d997168a9a2904c09ae
Ian Raburu <williamraburu@gmail.com>
2023-07-29 18:54:44 +0300

    Return a container including bot's profile

--- src/components/BotProfile.js
+++ src/components/BotProfile.js
@@ -2,7 +2,27 @@ import React, { useState } from 'react';
 import { useBotContext } from './BotContext';
 
 function BotProfile({ bot, enlisted, isInBotArmy  }) {
-    
+    const { enlistBot, unenlistBot, deleteBot } = useBotContext();
+
+    return (
+        <div className="bot-profile">
+            <img
+                className="avatar"
+                src={bot.avatar_url}
+                alt={bot.name}
+                onClick={handleClick}
+            />d
+            <h3>{bot.name}</h3>
+            <span>{bot.bot_class}</span>
+            <p>{bot.catchphrase}</p>
+
+            {isInBotArmy && (
+                <button className="delete-button" onClick={handleDelete}>
+                    x
+                </button>
+            )}
+        </div>
+    );
 };
 
 export default BotProfile;
