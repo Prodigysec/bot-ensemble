fa77e9f3d7d76be8191336a6e428f0a2921139aa
Ian Raburu <williamraburu@gmail.com>
2023-07-29 22:11:05 +0300

    Add sections for each component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import BotCollection from './components/BotCollection';
 import BotProvider from './components/BotContext';
-import BotArmy from './components/BotArmy'
-import { BotContext } from './components/BotContext'
+import BotArmy from './components/BotArmy';
+import { BotContext } from './components/BotContext';
+import './componentStyles/styles.css';
 
 function App() {
 
   return (
     <BotProvider>
-      <div>
-        <h1>Welcome to Bot-ensemble</h1>
+      <h1>Welcome to Bot-ensemble</h1>
+      <div className="container">
+        <div className="section1">
+          <h2>Your Bot Army</h2>
+          <BotArmy />
+        </div>
 
-        <h2>Your Bot Army</h2>
-        <BotArmy />
-
-        <h2>Bot Collection</h2>
-        <BotCollection />
+        <div className="section2">
+          <h2>Bot Collection</h2>
+          <BotCollection />
+        </div>
       </div>
     </BotProvider>
   )
