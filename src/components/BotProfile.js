b80c443bf99ed1c60a0edbaf6bbb44e73cf9cb9f
Ian Raburu <williamraburu@gmail.com>
2023-07-29 16:20:00 +0300

    Create BotProfile component

---
+++ src/components/BotProfile.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { useBotContext } from './BotContext';
+
+function BotProfile({ bot, enlisted, isInBotArmy  }) {
+    
+};
+
+export default BotProfile;
