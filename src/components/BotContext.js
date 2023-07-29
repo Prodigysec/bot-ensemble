430b645ad13493327d861e1bd31767aca1726d81
Ian Raburu <williamraburu@gmail.com>
2023-07-29 16:13:38 +0300

    Create context provider

--- src/components/BotContext.js
+++ src/components/BotContext.js
@@ -3,3 +3,10 @@ import { useState, useEffect, useContext } from 'react';
 
 // create context object
 const BotContext = createContext()
+
+// create context provider
+export default function BotProvider({ children }) {
+
+}
+
+export { BotContext };
