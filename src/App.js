5dd08308410949b40eb430dd1dbb8e86b2da8458
Ian Raburu <williamraburu@gmail.com>
2023-07-29 16:16:49 +0300

    Wrap components in BotProvider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import BotCollection from './components/BotCollection';
+import BotProvider from './components/BotContext';
+import BotArmy from './components/BotArmy'
+import { BotContext } from './components/BotContext'
 
 function App() {
+
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <BotProvider>
+      <div>
+        <h1>Welcome to Bot-ensemble</h1>
+
+        <h2>Your Bot Army</h2>
+        <BotArmy />
+
+        <h2>Bot Collection</h2>
+        <BotCollection />
+      </div>
+    </BotProvider>
+  )
 }
 
 export default App;
