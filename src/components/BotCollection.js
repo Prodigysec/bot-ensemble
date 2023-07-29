2a4bbdb92860da1024f4e97935a3026335a88288
Ian Raburu <williamraburu@gmail.com>
2023-07-29 16:18:36 +0300

    Create BotCollection component

---
+++ src/components/BotCollection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useContext, useState, useEffect } from "react";
+import BotProfile from './BotProfile';
+import { useBotContext } from './BotContext';
+
+function BotCollection() {
+    
+};
+
+export default BotCollection;
