f1dc5ca5a167c40707bc6358d107fdd9c6a0c502
Ian Raburu <williamraburu@gmail.com>
2023-07-29 16:14:21 +0300

    Add a check for using context within scope

--- src/components/BotContext.js
+++ src/components/BotContext.js
@@ -4,6 +4,14 @@ import { useState, useEffect, useContext } from 'react';
 // create context object
 const BotContext = createContext()
 
+export const useBotContext = () => {
+    const context = useContext(BotContext);
+    if (!context) {
+        throw new Error('useBotContext must be used within a BotProvider');
+    }
+    return context;
+};
+
 // create context provider
 export default function BotProvider({ children }) {
 
